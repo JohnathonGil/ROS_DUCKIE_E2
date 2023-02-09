670e471f1d9c9dffec029220b85fc995d96bd6be
banff0 <jaspereng00@gmail.com>
2023-02-09 14:43:54 -0700

    addded movement robustnesss

--- packages/odometry_package/src/moviment_planning_pub_sub_node.py
+++ packages/odometry_package/src/moviment_planning_pub_sub_node.py
@@ -40,6 +40,10 @@ class MovimentPlanningNode(DTROS):
         self.cmd_start_angle = 0
         self.cmd_start = 0
         self.vel = [0, 0]
+
+        self.forward_vel = [0.45, 0.4]
+        self.left = [-0.42, 0.42]
+        self.right = [0.42, -0.42]
         # Setup Service
 
         led_emitter = "/%s" % os.environ['VEHICLE_NAME'] + "/led_emitter_node/set_pattern"
@@ -162,19 +166,33 @@ class MovimentPlanningNode(DTROS):
         epsilon = 0.3
         print(f"NEAR: {self.dist}, GOAL: {dist}, DIFF: {abs(self.dist - dist)}")
         if abs(self.dist - dist) < epsilon or self.dist > dist:
+            print("#################CHANGING DIR")
             return True
         return False
 
     def angle_is_near(self, theta):
-        epsilon = 0.2
+        epsilon = 0.5
         rospy.loginfo(f"theta: {self.angle}, GOAL: {theta}, DIFF: {abs(self.angle - theta)}")
-        if abs(self.angle - theta) < epsilon or abs(self.angle) > abs(theta):
+        if abs(self.angle - theta) < epsilon :#or abs(self.angle) > abs(theta):
+            print("#################CHANGING SPEED")
             return True
         return False
 
     def run_cmd(self, cmd, state):
-        print(f"diff dist: {cmd['dist'] + self.cmd_start_dist}, diff angle: {cmd['angle'] + abs(self.cmd_start_angle)}")
-        return self.is_near(cmd["dist"] + self.cmd_start_dist) and self.angle_is_near(cmd["angle"] + abs(self.cmd_start_angle)) and self.state == state
+        print(f"diff dist: {cmd['dist'] + self.cmd_start_dist}, diff angle: {cmd['angle'] + self.cmd_start_angle}")
+        diff_angle = True
+        diff_dist = True
+
+        if cmd["angle"] != 0:
+            diff_angle = self.angle_is_near(cmd["angle"] + self.cmd_start_angle)
+        if cmd["dist"] != 0:
+            diff_dist = self.is_near(cmd["dist"] + self.cmd_start_dist)
+        return diff_dist and diff_angle and self.state == state
+
+    def pause(self):
+        self.vel = [0, 0]
+        self.set_velocity()
+        rospy.sleep(0.5)
 
                
     def run(self):
@@ -190,18 +208,18 @@ class MovimentPlanningNode(DTROS):
         # once these conditions are met |  do this  
 
             
-            {"dist": 0, "angle": 0, "vel": [0, 0], "col": "RED", "wait": 5},    # wait 5 secs
-            {"dist": 0, "angle": 0, "vel": [0.4, -0.4], "col": "BLUE"},         # turn right
-            {"dist": 0, "angle": -np.pi/2, "vel": [0.5, 0.5], "col": "BLUE"},   # go straight 
-            {"dist": 1.25, "angle": 0, "vel": [-0.4, 0.4], "col": "BLUE"},      # turn left
-            {"dist": 0, "angle": (-np.pi) / 2, "vel": [0.5, 0.5], "col": "BLUE"},    # go straight
-            {"dist": 1.25, "angle": 0,  "vel": [-0.4, 0.4], "col": "BLUE"},     #turn left
-            {"dist": 0, "angle": np.pi/2, "vel": [0.5, 0.5], "col": "BLUE"},    # go straight
+            #{"dist": 0, "angle": 0, "vel": [0, 0], "col": "RED", "wait": 5},    # wait 5 secs
+            {"dist": 0, "angle": 0, "vel": self.right, "col": "BLUE"},         # turn right
+            {"dist": 0, "angle": -np.pi/2, "vel": self.forward_vel, "col": "BLUE"},   # go straight 
+            {"dist": 1.25, "angle": 0, "vel": self.left, "col": "BLUE"},      # turn left
+            {"dist": 0, "angle": np.pi / 2, "vel": self.forward_vel, "col": "BLUE"},    # go straight
+            {"dist": 1.25, "angle": 0,  "vel": self.left, "col": "BLUE"},     #turn left
+            {"dist": 0, "angle": np.pi/2, "vel": self.forward_vel, "col": "BLUE"},    # go straight
             {"dist": 1.25, "angle": 0, "vel": [0, 0], "col": "RED", "wait": 5},    # wait 5 secs
-            {"dist": 0, "angle": 0,  "vel": [-0.4, 0.4], "col": "GREEN"},     #turn left
-            {"dist": 0, "angle": np.pi, "vel": [0.5, 0.5], "col": "GREEN"},    # go straight
-            {"dist": 0, "angle": 0, "vel": [0.4, -0.4], "col": "GREEN"},         # turn right
-            {"dist": 0, "angle": np.pi/2, "vel": [0, 0], "col": "LIGHT_OFF", "wait": 0}
+            {"dist": 0, "angle": 0,  "vel": self.left, "col": "GREEN"},     #turn left
+            {"dist": 0, "angle": np.pi, "vel": self.forward_vel, "col": "GREEN"},    # go straight
+            {"dist": 1.25, "angle": 0, "vel": self.right, "col": "GREEN"},         # turn right
+            {"dist": 0, "angle": -np.pi/2, "vel": [0, 0], "col": "LIGHT_OFF", "wait": 0}
 
         ]
 
@@ -209,6 +227,7 @@ class MovimentPlanningNode(DTROS):
         #self.set_velocity(*self.vel)
         curr_cmd = cmd[self.state-1]
         if self.run_cmd(curr_cmd, self.state) and not self.stop:
+            self.pause()
             self.cmd_start_dist = self.dist
             self.cmd_start_angle = self.angle
             self.cmd_start = rospy.Time.now().to_sec()
@@ -216,9 +235,8 @@ class MovimentPlanningNode(DTROS):
             print(curr_cmd)
             self.vel = curr_cmd["vel"]
             self.LED_emitor_client(curr_cmd["col"])
-            rospy.sleep(0.1)
+            self.set_velocity()
             if self.vel == [0, 0]:
-                self.set_velocity()
                 rospy.sleep(curr_cmd["wait"])
             print(f"EXECUTING STATE: {self.state-1}")
             if self.state > len(cmd):
