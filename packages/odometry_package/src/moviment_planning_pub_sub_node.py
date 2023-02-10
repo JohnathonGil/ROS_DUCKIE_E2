c5b7c2f866e19ba0e80ca3320f42cad86219eed5
banff0 <jaspereng00@gmail.com>
2023-02-10 13:54:42 -0700

    added comments to parts I did

--- packages/odometry_package/src/moviment_planning_pub_sub_node.py
+++ packages/odometry_package/src/moviment_planning_pub_sub_node.py
@@ -33,27 +33,24 @@ class MovimentPlanningNode(DTROS):
         self.run_led = 0
         self.goal_reached = [True, False, False]
         
+        # vars to track movment
         self.state = 1
         self.stop = False
         self.running = True
         self.cmd_start_dist = 0
         self.cmd_start_angle = 0
-        self.cmd_start = 0
+        self.cmd_start = 0        # prob dont need this one, it just tracks time
         self.vel = [0, 0]
 
+        ############################################# CHANGE THIS IF ON A DIFFERENT DUCKIEBOT
         self.forward_vel = [0.5, 0.57]
         self.left = [-0.4, 0.4]
         self.right = [0.4, -0.4]
+        ############################################# THIS IS SET FOR csc22904
 
         led_emitter = "/%s" % os.environ['VEHICLE_NAME'] + "/led_emitter_node/set_pattern"
         rospy.wait_for_service(led_emitter)
         self.change_pattern = rospy.ServiceProxy(led_emitter, ChangePattern)
-
-        
-
-
-        # self.current_pattern_name = "LIGHT_OFF"
-        # self.changePattern(self.current_pattern_name)
 
     #Subscriber
         robot_pose = "/%s" % os.environ['VEHICLE_NAME'] + "/wheel_odometry/pose"
@@ -67,7 +64,6 @@ class MovimentPlanningNode(DTROS):
 
         self.exit_pub = rospy.Publisher("/exit", Bool, queue_size=1)
 
-        #os.system("dts duckiebot demo --demo_name led_emitter_node --duckiebot_name $BOT --package_name led_emitter --image duckietown/dt-core:daffy-arm64v8")
 
         self.log("Initialized")
 
@@ -77,42 +73,25 @@ class MovimentPlanningNode(DTROS):
 
         self.distance_reached = self.dist - self.starting_distance
         self.angle_reached = self.angle - self.starting_angle
-        # rospy.loginfo("The encoder angle measurement: " + str(self.angle))
-        # rospy.loginfo("The starting angle measurement: " + str(self.starting_angle))
-        # rospy.loginfo("The current angle measurement: " + str(self.angle_reached))
-        #rospy.loginfo("Distance: " + str(self.distance_reached))
-        #self.execution_time = data.header.stamp
-        #rospy.loginfo("Execution Time: " + str(self.execution_time))
-        
-    # def led_server(self):
-    #     led_color_setter = "/%s" % os.environ['VEHICLE_NAME'] + "/led_emitter_node/set_pattern"
-    #     rospy.init_node(led_color_setter)
-    #     self.srv_set_pattern_ = rospy.Service(led_color_setter, ChangePattern, self.srvSetPattern)
-
-    # def srvSetPattern(self, msg):
-    #     """Changes the current pattern according to the pattern name sent in the message.
-    #     Args:
-    #         msg (String): requested pattern name
-    #     """
-    #     self.changePattern(str(msg.pattern_name.data))
-    #     return ChangePatternResponse()
     
     def LED_emitor_client(self, pattern_name):
         color = String()
         color.data = pattern_name
         self.change_pattern(color)
       
+    # publish the velocity to a topic
     def set_velocity(self):
 
         msg = WheelsCmdStamped()
         msg.vel_left = self.vel[0]
         msg.vel_right = self.vel[1]
         self.pub_wheel_command.publish(msg)
-
+        # stop the main loop if we are done
         if self.stop:
             self.running = False
             rospy.signal_shutdown("ROBO MOVED")
-
+    
+    # lets other nodes know to shutdown
     def publish_exit(self):
         msg  = Bool()
         msg.data = self.stop
@@ -120,17 +99,20 @@ class MovimentPlanningNode(DTROS):
 
         
 
+    # can we delete this?
     def set_initial_distance(self):
         
         if self.set_start_dist:
             self.starting_distance = self.dist
             self.set_start_dist = False
 
+    # can we delete this?
     def set_initial_angle(self):
         if self.set_start_angle:
             self.starting_angle = self.angle
             self.set_start_angle = False
 
+    # can we delete this?
     def LED_state(self, Color1, Color2, switch):
         if switch == 0:
             time = 5
@@ -139,7 +121,7 @@ class MovimentPlanningNode(DTROS):
             self.LED_emitor_client(Color2)
             self.run_led = 1
 
-
+    # can we delete this?
     def turn(self, direction):
         self.set_initial_angle()
         if direction == "right":
@@ -161,22 +143,29 @@ class MovimentPlanningNode(DTROS):
         elif self.distance_reached > 1.22:
                 self.set_velocity(0.0, 0.0)
         
+    # check if the bot has come close to moving a dist
     def is_near(self, dist):
+        # threshold
         epsilon = 0.3
         print(f"NEAR: {self.dist}, GOAL: {dist}, DIFF: {abs(self.dist - dist)}")
+        # check if the bot is near or has pasted a dist
         if abs(self.dist - dist) < epsilon or self.dist > dist:
             print("#################CHANGING DIR")
             return True
         return False
 
+    # check if the bot has come close to an angle
     def angle_is_near(self, theta):
+        # threshold
         epsilon = 0.3
         rospy.loginfo(f"theta: {self.angle}, GOAL: {theta}, DIFF: {abs(self.angle - theta)}")
+        # check if the bot is near an angle
         if abs(self.angle - theta) < epsilon :#or abs(self.angle) > abs(theta):
             print("#################CHANGING SPEED")
             return True
         return False
 
+    # checks if stopping conditions are met for a cmd
     def run_cmd(self, cmd, state):
         print(f"diff dist: {cmd['dist'] + self.cmd_start_dist}, diff angle: {cmd['angle'] + self.cmd_start_angle}")
         diff_angle = True
@@ -188,6 +177,7 @@ class MovimentPlanningNode(DTROS):
             diff_dist = self.is_near(cmd["dist"] + self.cmd_start_dist)
         return diff_dist and diff_angle and self.state == state
 
+    # wait before each cmd for 1 second
     def pause(self):
         self.vel = [0, 0]
         self.set_velocity()
@@ -195,19 +185,10 @@ class MovimentPlanningNode(DTROS):
 
                
     def run(self):
-        #self.LED_state("RED","GREEN",self.run_led)
 
-        #self.set_initial_distance()
-        # if self.angle_reached == 0:
-        #     self.set_velocity(0.4, -0.4)
-        # elif self.angle_reached < -(np.pi/2)+0.4:
-        #     self.set_velocity(0.0, 0.0)
-
+        # robo will execute all these commands in order until termination           read these descriptions
         cmd = [
             # once these conditions are met |  do this  
-
-            
-
             {"dist": 0, "angle": 0, "vel": [0, 0], "col": "RED", "wait": 5},        # wait 5 secs
             {"dist": 0, "angle": 0, "vel": self.right, "col": "BLUE"},              # turn right
             {"dist": 0, "angle": -np.pi/2, "vel": self.forward_vel, "col": "BLUE"}, # go straight 
@@ -231,21 +212,22 @@ class MovimentPlanningNode(DTROS):
             {"dist": 0, "angle": 0, "vel": [0, 0], "col": "LIGHT_OFF", "wait": 0},  # alt terminate code (cant be used w/ circle)
         ]
 
-
-        #self.set_velocity(*self.vel)
         curr_cmd = cmd[self.state-1]
-
         print(self.angle_is_near(2*np.pi + self.cmd_start_angle), not self.is_near(self.cmd_start_dist))    
 
+        # runs cicle cmd and cirlce exit (ones that are stings)
         if curr_cmd == "circle":
             print("CIRCLE_START", self.dist)
+            ############################################# CHANGE THIS IF ON A DIFFERENT DUCKIEBOT
             self.vel = [0.3, 0.6]
+            ############################################# THIS IS SET FOR csc22904
             self.cmd_start_dist = self.dist
             self.cmd_start_angle = self.angle
             self.state += 1
             self.LED_emitor_client("WHITE")
             print(curr_cmd)
-        
+
+        # runs cicle cmd and cirlce exit (ones that are stings)
         elif curr_cmd == "end" and self.angle_is_near(2*np.pi + self.cmd_start_angle) and not self.dist == self.cmd_start_dist + 0.2:
             print("CIRCEL_END", self.dist)
             print(curr_cmd)
@@ -254,46 +236,28 @@ class MovimentPlanningNode(DTROS):
             self.set_velocity()
             self.stop = True
 
+        # this runs all other cmds (ones that are dicts)
         elif type(curr_cmd) != str and self.run_cmd(curr_cmd, self.state) and not self.stop:
+            # pause between cmds
             self.pause()
+            # set the init vals for each cmd
             self.cmd_start_dist = self.dist
             self.cmd_start_angle = self.angle
             self.cmd_start = rospy.Time.now().to_sec()
+            # update which cmd we are on
             self.state += 1
             print(curr_cmd)
+            # publish the velocity and LED col
             self.vel = curr_cmd["vel"]
             self.LED_emitor_client(curr_cmd["col"])
             self.set_velocity()
+            # check if we are waiting then sleep 
             if self.vel == [0, 0]:
                 rospy.sleep(curr_cmd["wait"])
             print(f"EXECUTING STATE: {self.state-1}")
+            # terminate once all cmds are run
             if self.state > len(cmd):
                 self.stop = True
-
-
-        
-
-
-
-            # if self.is_near(0) and self.state == 1:
-            #     self.cmd_start_dist = self.dist
-            #     self.state += 1
-            #     self.set_velocity(0.6, 0.6)
-            #     self.LED_emitor_client("RED")
-            #     print("GOING STTROIGH")
-            # elif self.is_near(1.25) and self.state == 2:
-            #     self.state += 1
-            #     self.set_velocity(-0.6, 0.6)
-            #     self.LED_emitor_client("BLUE")
-            #     print("TURNING")
-            # elif self.angle_is_near(np.pi/2) and self.state == 3:
-            #     self.state += 1
-            #     self.set_velocity(0.0, 0.0)
-            #     self.stop = True
-            #     self.LED_emitor_client("GREEN")
-            #     print("STOPPING")
-
-           
 
 if __name__ == '__main__':
     # create the node
